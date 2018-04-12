e49e5112674b139addb2a037f7fcaacc91d24cc3
tgni <nitonggui@aliyun.com>
2018-04-12 09:39:43 +0800

    aaa

--- src/tree/avl/avltree.h
+++ src/tree/avl/avltree.h
@@ -3,24 +3,26 @@ typedef int ElementType;
 #ifndef _AvlTree_H
 #define _AvlTree_H
 
+#include "types.h"
+
 struct AvlNode;
 typedef struct AvlNode *Position;
 typedef struct AvlNode *AvlTree;
 
-struct AvlNode
-{
-    ElementType Element;
-    AvlTree  Left;
-    AvlTree  Right;
-    int      Height;
-};
-
-AvlTree MakeEmpty( AvlTree T );
-Position Find( ElementType X, AvlTree T );
-Position FindMin( AvlTree T );
-Position FindMax( AvlTree T );
-AvlTree Insert( ElementType X, AvlTree T );
-AvlTree Delete( ElementType X, AvlTree T );
-ElementType Retrieve( Position P );
+extern AvlTree MakeEmpty( AvlTree T );
+extern Position Find( ElementType X, AvlTree T );
+extern Position FindMin( AvlTree T );
+extern Position FindMax( AvlTree T );
+extern AvlTree Insert( ElementType X, AvlTree T );
+extern AvlTree LoopInsert( ElementType X, AvlTree T );
+extern AvlTree Delete( ElementType X, AvlTree T );
+extern ElementType Retrieve( Position P );
+extern void ComputeDistAndDepth( AvlTree T );
+extern void VPrintAvlTree(AvlTree T);
+extern void HPrintAvlTree(AvlTree T);
+extern void InOrder(AvlTree T);
+extern void PreOrder(AvlTree T);
+extern void PostOrder(AvlTree T);
+extern int32_t GetHeight(AvlTree T);
 
 #endif  /* _AvlTree_H */
