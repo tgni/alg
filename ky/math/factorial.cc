7642f4c1cb82213d9261ede9f50a4cec47707d26
tgni <tgni@aliyun.com>
2019-01-14 10:07:03 +0800

    delete

--- ky/math/factorial.cc
+++
@@ -1,9 +0,0 @@
-/* given N, output N's factorial. N is less than 1000.
- */
-
-#include <stdio.h>
-
-int main()
-{
-	return 0;
-}
--- src/scheduler/sched.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SCHED_H_
-#define _SCHED_H_
-
-typedef struct queue_s {
-	uint32_t nr;
-	list_head_t hdr;
-} queue_t;
-
-#endif /* _SCHED_H_ */
