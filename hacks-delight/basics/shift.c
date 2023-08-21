1ebf7084e03ed7a81ec740ea01b9392a33dd55d3
tgni <nitonggui@gmail.com>
2023-08-21 14:58:09 +0800

    add hacks-delight directory

---
+++ hacks-delight/basics/shift.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_binary(int x)
+{
+	int i;
+
+	for (i = 31; i >= 0; --i) {
+		printf("%d", (x >> i) & 1);
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc < 2) {
+		printf("usage: ./shift [x]\n");
+		return -1;
+	}
+
+	x = strtoul(argv[1], NULL, 0);
+	printf("x:");
+	print_binary(x);
+	printf("x & (x-1):");
+	print_binary(x & (x-1));
+	printf("x | (x+1):");
+	print_binary(x | (x+1));
+	printf("x & (x+1):");
+	print_binary(x & (x+1));
+	printf("x | (x-1):");
+	print_binary(x | (x-1));
+
+	return 0;
+}
